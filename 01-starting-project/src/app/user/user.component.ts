ef183cc2a0cbddc44e0ffc89587c725d3e0ad92f
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-08-14 22:29:37 +0100

    Custom Event with Output decorator and EventEmitter

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,28 +1,27 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, EventEmitter, Input, Output, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
 let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  avatarPath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  // get avatarPath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
-  
+  @Output() selectedUser = new EventEmitter<void>();
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
   onSelectUser() {
-    randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    // this.selectedUser = DUMMY_USERS[randomIndex];
+    
   }
 }
