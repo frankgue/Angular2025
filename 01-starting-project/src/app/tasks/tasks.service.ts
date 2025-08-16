69a1f73c827873bfbeb0af857e2d70735e649fac
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-08-16 19:49:26 +0100

    adding LocalStorage

---
+++ 01-starting-project/src/app/tasks/tasks.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { NewTaskData } from './task/task.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  private tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ];
+
+  constructor() {
+    // Initialize the service, if needed
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
+  getUserTasks(userId: string) {
+    return this.tasks.filter((task) => task.userId === userId);
+  }
+
+  addTask(task: NewTaskData, userId: string) {
+    const newTask = {
+      id: new Date().getTime().toString(),
+      userId: userId,
+      ...task,
+    };
+    this.tasks.unshift(newTask);
+    this.saveTasksToLocalStorage();
+    // return newTask;
+  }
+
+  removeTask(taskId: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasksToLocalStorage();
+  }
+
+  private saveTasksToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+}
