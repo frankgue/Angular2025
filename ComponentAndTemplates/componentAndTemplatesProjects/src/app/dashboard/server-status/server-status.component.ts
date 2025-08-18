f40001fb5bf6cb4e9d220796acf5a497d0874c01
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-08-18 18:03:36 +0100

    start lifecycle interface

--- ComponentAndTemplates/componentAndTemplatesProjects/src/app/dashboard/server-status/server-status.component.ts
+++ ComponentAndTemplates/componentAndTemplatesProjects/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,10 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
   host: {
-    id: "status"
-  }
+    id: 'status',
+  },
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
-  currentStatus = 'online';
+  constructor() {}
+
+  ngOnInit(): void {
+    setInterval(() => {
+      const random = Math.random(); // 0 - 0.9999999999999999999
+
+      if (random < 0.5) {
+        this.currentStatus = 'online';
+      } else if (random < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
--- ComponentAndTemplates/componentAndTemplatesProjects/src/app/shared/control/control.component.ts
+++ ComponentAndTemplates/componentAndTemplatesProjects/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,9 +8,26 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': "onClick()"
   }
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+
+  // @HostListener('click') onClick(event: MouseEvent) {
+  //   console.log('Control clicked', event);
+  // }
+
   label = input.required<string>();
+
+  // private hostEl = inject(ElementRef); 
+
+  onClick(){
+    console.log("Control Click!!");
+    // console.log(this.hostEl);
+    
+    // Add your click handling logic here
+  }
+
 }
