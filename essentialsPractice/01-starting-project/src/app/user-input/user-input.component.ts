ce0cd2885a78e1542e06c82d3fd6c205ba3967cb
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-08-18 01:31:43 +0100

    Investmant calculator using Service and signal with Module

--- essentialsPractice/01-starting-project/src/app/user-input/user-input.component.ts
+++ essentialsPractice/01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,13 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { InvestmentService } from '../investment.service';
-import type { AnnualData } from '../annual-data.model';
 import type { UserInvestmentInput } from '../user-investment-input.model';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
 })
